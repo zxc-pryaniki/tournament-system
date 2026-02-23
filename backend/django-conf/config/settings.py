bcd71e10741ff613775f0a305cb35c2b91c59d75
Bohdan Ilchuk <ilchuk.bohdan.r@gmail.com>
2026-02-23 22:40:53 +0200

    PostgreSQL docker setup

--- backend/django-conf/config/settings.py
+++ backend/django-conf/config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tournament_db',
+        'USER': 'user',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
