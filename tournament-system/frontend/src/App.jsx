720c8e059638353efe88d01d98e8e866252e746a
Dav1ddicks <narykhniuk_davyd@gymnasia21.lutsk.ua>
2026-03-29 15:38:22 +0300

    Добавив редактор тексту в описі адмін меню

--- tournament-system/frontend/src/App.jsx
+++ tournament-system/frontend/src/App.jsx
@@ -5,13 +5,10 @@ export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      
-      {
-      }
+    <div className="min-h-screen bg-gray-100 p-6 flex justify-start items-start">
       <button
         onClick={() => setIsModalOpen(true)}
-        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg shadow-lg transition-all"
+        className="bg-[#2563eb] hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg shadow-lg transition-all"
       >
         Створити турнір
       </button>
